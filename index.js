925d33947ec2bbaa96637dc1b41c0aea24e24adb
Abhishek Pathak <pathakabhishek11182001@gmail.com>
2023-06-15 08:55:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-// const firstString = "upendra";
-// const secondString = "Abhishek";
-// const thirdString = "Amit";
-
-// if(firstString.length < secondString.length && firstString.length < thirdString.length){
-//     console.log(`${firstString} is smallest string`);
-// }else if(secondString.length < firstString.length && secondString.length < thirdString.length){
-//     console.log(`${secondString} is smallest string`);
-// }
-// else{
-//     console.log(`${thirdString} is smallest`)
-// }
 
  const readlineSync = require('readline-sync');
  const number = +(readlineSync.question("Please Enter Your Any Number:"));
